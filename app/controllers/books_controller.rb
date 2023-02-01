340c6565e8de3088c91ca1abc398943d4e56799c
EC2 Default User <ec2-user@ip-172-31-3-94.ap-northeast-1.compute.internal>
2023-02-01 08:51:01 +0000

    [Add]いいね機能の非同期化

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @book_new = Book.new
     @book_comment = BookComment.new
+    @user = @book.user
   end
 
   def index

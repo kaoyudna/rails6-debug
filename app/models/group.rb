ba33fa7d7828e9ca0861682068ce88c945236a57
EC2 Default User <ec2-user@ip-172-31-43-118.ap-northeast-1.compute.internal>
2023-02-04 13:31:48 +0000

    [Add]中断

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,4 +2,10 @@ class Group < ApplicationRecord
 
   has_many :group_users
   has_many :users, through: :group_users
+
+  has_one_attached :image
+
+  def get_group_image
+    (image.attached?) ? image : 'no_image.jpg'
+  end
 end
